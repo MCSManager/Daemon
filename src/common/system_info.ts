5d5e3c79a74d01bb5d0f85054de45b3de41b9b22
Suwings <suwings@outlook.com>
2021-12-25 15:50:46 +0800

    修复 内存获取方式

--- src/common/system_info.ts
+++ src/common/system_info.ts
@@ -1,12 +1,13 @@
 /*
  * @Author: Copyright 2021 Suwings
  * @Date: 2021-08-02 11:03:31
- * @LastEditTime: 2021-08-02 13:26:30
+ * @LastEditTime: 2021-12-25 15:50:21
  * @Description:
  */
 
 import os from "os";
 import osUtils from "os-utils";
+import systeminformation from "systeminformation";
 
 interface ISystemInfo {
   cpuUsage: number;
@@ -21,29 +22,38 @@ interface ISystemInfo {
   cwd: string;
   processCpu: number;
   processMem: number;
+  loadavg: number[];
 }
 
 // 系统详细信息每一段时间更新一次
-let systemResources: ISystemInfo = null;
+let info: ISystemInfo = {
+  type: os.type(),
+  hostname: os.hostname(),
+  platform: os.platform(),
+  release: os.release(),
+  uptime: os.uptime(),
+  cwd: process.cwd(),
+  loadavg: os.loadavg(),
+  freemem: 0,
+  cpuUsage: 0,
+  memUsage: 0,
+  totalmem: 0,
+  processCpu: 0,
+  processMem: 0
+};
+
+// 定时刷新缓存
 setInterval(() => {
-  osUtils.cpuUsage((p) => {
-    systemResources = {
-      cpuUsage: p,
-      totalmem: os.totalmem(),
-      freemem: os.freemem(),
-      type: os.type(),
-      hostname: os.hostname(),
-      platform: os.platform(),
-      release: os.release(),
-      uptime: os.uptime(),
-      cwd: process.cwd(),
-      processCpu: process.cpuUsage().system,
-      processMem: process.memoryUsage().rss,
-      memUsage: (os.totalmem() - os.freemem()) / os.totalmem()
-    };
+  systeminformation.mem((data) => {
+    info.freemem = data.available;
+    info.totalmem = data.total;
+    info.memUsage = (data.total - data.available) / data.total;
   });
-}, 1000);
+  osUtils.cpuUsage((p) => {
+    info.cpuUsage = p;
+  });
+}, 2000);
 
 export function systemInfo() {
-  return systemResources;
+  return info;
 }
--- src/routers/info_router.ts
+++ src/routers/info_router.ts
@@ -1,35 +1,15 @@
 /*
- * @Projcet: MCSManager Daemon
- * @Author: Copyright(c) 2020 Suwings
-
- * @Description: 应用实例相关控制器
+ * @Author: Suwings
+ * @Date: 2021-12-05 15:43:50
+ * @LastEditTime: 2021-12-25 15:49:43
  */
-import os from "os";
-import osUtils from "os-utils";
 
 import * as protocol from "../service/protocol";
 import { routerApp } from "../service/router";
 import InstanceSubsystem from "../service/system_instance";
 import Instance from "../entity/instance/instance";
 
-// 系统详细信息每一段时间更新一次
-var systemResources: any = null;
-setInterval(() => {
-  osUtils.cpuUsage((p) => {
-    systemResources = {
-      cpuUsage: p,
-      totalmem: os.totalmem(),
-      freemem: os.freemem(),
-      type: os.type(),
-      // cpus: os.cpus(),
-      hostname: os.hostname(),
-      loadavg: os.loadavg(),
-      platform: os.platform(),
-      release: os.release(),
-      uptime: os.uptime()
-    };
-  });
-}, 1000);
+import { systemInfo } from "../common/system_info";
 
 // 获取守护进程系统基本信息
 routerApp.on("info/overview", async (ctx) => {
@@ -49,7 +29,7 @@ routerApp.on("info/overview", async (ctx) => {
       running,
       total
     },
-    system: systemResources
+    system: systemInfo()
   };
   protocol.response(ctx, info);
 });
