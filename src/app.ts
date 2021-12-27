db06607900bbdb18ecf4b49391ae14f1f9a3097f
Suwings <suwings@outlook.com>
2021-12-27 21:40:41 +0800

    更新 文件标头

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-/**
+/*
   Copyright (C) 2021 Suwings(https://github.com/Suwings)
 
   This program is free software: you can redistribute it and/or modify
@@ -11,17 +11,11 @@
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
   版权所有 (C) 2021 Suwings(https://github.com/Suwings)
 
-  本程序为自由软件，你可以依据 GPLv3 的条款，再分发和/或修改它。
+  本程序为自由软件，你可以依据 GPL 的条款（第三版或者更高），再分发和/或修改它。
   该程序以具有实际用途为目的发布，但是并不包含任何担保，
   也不包含基于特定商用或健康用途的默认担保。具体细节请查看 GPL 协议。
-
-  你在收到本程序的同时，也应当收到一份GNU通用公共授权许可证副本，
-  如果没有，请查看<http://www.gnu.org/licenses/>。
 */
 
 import { getVersion, initVersionManager } from "./service/version";
