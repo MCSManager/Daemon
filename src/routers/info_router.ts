181642e71bd11a41cd5e638a687787cd3a5020f6
Suwings <Suwings@outlook.com>
2022-01-27 17:36:45 +0800

    新增 传递守护进程版本

--- src/routers/info_router.ts
+++ src/routers/info_router.ts
@@ -31,9 +31,12 @@ import InstanceSubsystem from "../service/system_instance";
 import Instance from "../entity/instance/instance";
 
 import { systemInfo } from "../common/system_info";
+import { getVersion } from "../service/version";
+
 
 // 获取守护进程系统基本信息
 routerApp.on("info/overview", async (ctx) => {
+  const daemonVersion = getVersion();
   let total = 0;
   let running = 0;
   InstanceSubsystem.instances.forEach((v) => {
@@ -41,6 +44,7 @@ routerApp.on("info/overview", async (ctx) => {
     if (v.status() == Instance.STATUS_RUNNING) running++;
   });
   const info = {
+    version: daemonVersion,
     process: {
       cpu: process.cpuUsage().system,
       memory: process.memoryUsage().heapUsed,
