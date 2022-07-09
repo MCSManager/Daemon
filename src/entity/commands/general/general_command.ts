79a8f76295fc52923f5c6b3e54de8aa5dfd4a03e
Suwings <Suwings@outlook.com>
2022-07-09 13:09:27 +0800

    Fix: 正常命令执行

--- src/entity/commands/general/general_command.ts
+++ src/entity/commands/general/general_command.ts
@@ -32,8 +32,8 @@ export default class GeneralSendCommand extends InstanceCommand {
     // 关服命令需要发送命令，但关服命令执行前会设置状态为关闭中状态。
     // 所以这里只能通过进程是否存在来执行命令
     if (!instance.process) instance.failure(new Error("命令执行失败，因为实例实际进程不存在."));
-    instance.process.write(buf);
-    // instance.process.write(encode(buf, instance.config.oe));
-    // instance.process.write("\r\n");
+    // instance.process.write(buf);
+    instance.process.write(encode(buf, instance.config.oe));
+    instance.process.write("\n");
   }
 }
