bf06c9c996c2457281b6e2b4300e83f9024b34a0
unitwk <unitwk@outlook.com>
2022-08-21 10:00:32 +0800

    Feat: auto install

--- src/service/install.ts
+++ src/service/install.ts
@@ -6,15 +6,12 @@ import fs from "fs-extra";
 import https from "https";
 import path from "path";
 import logger from "./log";
-const PTY_PATH = path.normalize(path.join(process.cwd(), "lib", "pty"));
+const PTY_NAME = `pty_${os.platform()}_${os.arch()}${os.platform() === "win32" ? ".exe" : ""}`;
+const PTY_PATH = path.normalize(path.join(process.cwd(), "lib", PTY_NAME));
 const PTY_DIR_PATH = path.join(process.cwd(), "lib");
 
 function installPtyForLinux(url: string) {
   return new Promise<number>((resolve, reject) => {
-    if (os.arch() !== "x64") {
-      if (!fs.existsSync(PTY_PATH)) logger.info($t("install.ptyNotSupportSystem"));
-      resolve(-1);
-    }
     if (!fs.existsSync(PTY_DIR_PATH)) fs.mkdirsSync(PTY_DIR_PATH);
     if (fs.existsSync(PTY_PATH) && fs.statSync(PTY_PATH)?.size > 1024) {
       logger.info($t("install.ptySupport"));
@@ -47,7 +44,7 @@ function installPtyForLinux(url: string) {
 // Reference: https://github.com/MCSManager/pty
 export function initDependent() {
   if (os.platform() !== "linux") return logger.info($t("install.skipInstall"));
-  const ptyUrls = ["https://mcsmanager.com/download/pty_linux", "https://mcsmanager.oss-cn-guangzhou.aliyuncs.com/pty_linux"];
+  const ptyUrls = [`https://mcsmanager.com/download/${PTY_NAME}`, `https://mcsmanager.oss-cn-guangzhou.aliyuncs.com/${PTY_NAME}`];
   function setup(index = 0) {
     installPtyForLinux(ptyUrls[index])
       .then(() => {
