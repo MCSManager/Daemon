81baa8dfbf63f07da32bba5c054547b325412840
糯米 <92624354+nuomiaa@users.noreply.github.com>
2022-10-03 08:20:45 +0800

    错误的拼写

--- src/entity/minecraft/mc_getplayer.ts
+++ src/entity/minecraft/mc_getplayer.ts
@@ -2,7 +2,7 @@
 
 import Instance from "../instance/instance";
 import InstanceCommand from "../commands/base/command";
-import MCServStatus from "../../common/mcping";
+import MCServerStatus from "../../common/mcping";
 
 export default class MinecraftGetPlayersCommand extends InstanceCommand {
   constructor() {
@@ -11,7 +11,7 @@ export default class MinecraftGetPlayersCommand extends InstanceCommand {
 
   async exec(instance: Instance) {
     if (instance.config.pingConfig.ip && instance.config.pingConfig.port) {
-      const player = await new MCServStatus(instance.config.pingConfig.port, instance.config.pingConfig.ip).getStatus();
+      const player = await new MCServerStatus(instance.config.pingConfig.port, instance.config.pingConfig.ip).getStatus();
       return player;
     }
     return null;
