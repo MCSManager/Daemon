0a92fa3b6b7f66c177777aa903bb372ec8c5ee9b
unitwk <unitwk@outlook.com>
2022-10-23 20:42:23 +0800

    Feat: add hiper yml support

--- src/service/async_task_service/hiper_start.ts
+++ src/service/async_task_service/hiper_start.ts
@@ -54,6 +54,21 @@ export class HiPerTask extends EventEmitter implements IAsyncTask {
 
       // TODO: The node information in point.yml is overwritten to key.yml
       // fs.writeFile()
+      let keyFile = fs.readFileSync(this.KEY_YML, "utf-8");
+      const pointFile = fs.readFileSync(this.POINT_YML, "utf-8");
+      const START_TEXT = ">>> AUTO SYNC AREA";
+      const END_TEXT = "<<< AUTO SYNC AREA";
+      const start = pointFile.indexOf(START_TEXT);
+      const end = pointFile.indexOf(END_TEXT);
+      if (start > -1 && end > -1) {
+        const nodesText = pointFile.slice(start, end);
+        const p1 = keyFile.indexOf(START_TEXT);
+        const p2 = keyFile.indexOf(END_TEXT);
+        if (p1 >= 0 || p2 >= 0) {
+          keyFile = keyFile.replace(keyFile.slice(p1, p2), "");
+        }
+        keyFile += "\n" + nodesText;
+      }
 
       // Start Command: hiper.exe -config .\key.yml
       HiPer.openHiPer(this.keyYmlPath);
