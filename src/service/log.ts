27366bbf3140595ddcd8a76f53ffd04026297474
unitwk <unitwk@outlook.com>
2022-11-17 17:53:11 +0800

    Feat: add sysinfo log

--- src/service/log.ts
+++ src/service/log.ts
@@ -2,6 +2,9 @@
 
 import fs from "fs-extra";
 import * as log4js from "log4js";
+import os from "os";
+import { systemInfo } from "../common/system_info";
+import { $t } from "../i18n";
 
 const LOG_FILE_PATH = "logs/current.log";
 
@@ -41,5 +44,25 @@ log4js.configure({
 });
 
 const logger = log4js.getLogger("default");
+
+function toInt(v: number) {
+  return parseInt(String(v));
+}
+
+function systemInfoReport() {
+  const MB_SIZE = 1024 * 1024;
+  const info = systemInfo();
+
+  const self = process.memoryUsage();
+  const sysInfo =
+    `MEM: ${toInt((info.totalmem - info.freemem) / MB_SIZE)}MB/${toInt(info.totalmem / MB_SIZE)}MB` + ` CPU: ${toInt(info.cpuUsage * 100)}%`;
+  const selfInfo = `Heap: ${toInt(self.heapUsed / MB_SIZE)}MB/${toInt(self.heapTotal / MB_SIZE)}MB`;
+  const selfInfo2 = `RSS: ${toInt(self.rss / MB_SIZE)}MB External: ${toInt(self.external / MB_SIZE)}MB `;
+  const logTip = $t("app.sysinfo");
+  logger.info([`[${logTip}]`, sysInfo].join(" "));
+  logger.info([`[${logTip}]`, selfInfo, selfInfo2].join(" "));
+}
+
+setInterval(systemInfoReport, 1000 * 15);
 
 export default logger;
