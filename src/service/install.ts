3d18acfd0294a0798aa89bfea12562623c95865b
unitwk <unitwk@outlook.com>
2023-01-02 14:39:11 +0800

    Feat: pty chmod operation

--- src/service/install.ts
+++ src/service/install.ts
@@ -11,10 +11,22 @@ const PTY_NAME = `pty_${os.platform()}_${os.arch()}${os.platform() === "win32" ?
 const PTY_PATH = path.normalize(path.join(process.cwd(), "lib", PTY_NAME));
 const PTY_DIR_PATH = path.join(process.cwd(), "lib");
 
-function installPtyForLinux(url: string) {
+function ptyChmod() {
+  try {
+    fs.chmodSync(PTY_PATH, 0o777);
+    return true;
+  } catch (error) {
+    logger.warn($t("install.changeModeErr", { path: PTY_PATH }));
+    fs.remove(PTY_PATH, () => {});
+    return false;
+  }
+}
+
+function installPty(url: string) {
   return new Promise<number>((resolve, reject) => {
     if (!fs.existsSync(PTY_DIR_PATH)) fs.mkdirsSync(PTY_DIR_PATH);
     if (fs.existsSync(PTY_PATH) && fs.statSync(PTY_PATH)?.size > 1024) {
+      if (!ptyChmod()) return reject(new Error("ptyChmod error"));
       logger.info($t("install.ptySupport"));
       return resolve(1);
     }
@@ -42,13 +54,11 @@ export function initDependent() {
   // if (os.platform() !== "linux") return logger.info($t("install.skipInstall"));
   const ptyUrls = [`https://mcsmanager.oss-cn-guangzhou.aliyuncs.com/${PTY_NAME}`];
   function setup(index = 0) {
-    installPtyForLinux(ptyUrls[index])
+    installPty(ptyUrls[index])
       .then(() => {
         logger.info($t("install.installed"));
         logger.info($t("install.guide"));
-        fs.chmod(PTY_PATH, 0o777, (err) => {
-          if (err) logger.warn($t("install.changeModeErr", { path: PTY_PATH }));
-        });
+        ptyChmod();
       })
       .catch((err: Error) => {
         fs.remove(PTY_PATH, () => {});
