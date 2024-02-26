c8f07c02714069e02f1c6a6544a17844f89c075f
Johnathan brown <john1234brown23@gmail.com>
2024-02-26 06:40:52 -0500

    Update cmd.ts
    Basic Sanitization Example

--- src/entity/commands/cmd.ts
+++ src/entity/commands/cmd.ts
@@ -2,16 +2,25 @@
 
 import Instance from "../instance/instance";
 import InstanceCommand from "./base/command";
+import { sanitizeCommand } from "./sanitize"; // assuming you have a sanitize function
 
 export default class SendCommand extends InstanceCommand {
-  public cmd: string;
+  private _cmd: string;
 
   constructor(cmd: string) {
     super("SendCommand");
     this.cmd = cmd;
   }
 
+  public set cmd(cmd: string) {
+    this._cmd = sanitizeCommand(cmd);
+  }
+
+  public get cmd(): string {
+    return this._cmd;
+  }
+
   async exec(instance: Instance) {
-    return await instance.execPreset("command", this.cmd);
+    return await instance.execPreset("command", this._cmd);
   }
 }
