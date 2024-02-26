4452ab3bec876da7603719d4481cef932ce1c11b
Johnathan brown <john1234brown23@gmail.com>
2024-02-26 06:51:44 -0500

    Update sanitize.js
    More Complexxx Sanitization Method

--- src/entity/commands/sanitize.js
+++ src/entity/commands/sanitize.js
@@ -1,12 +1,18 @@
 export function sanitizeCommand(command: string): string {
-  // Escape special characters that have special meaning in regular expressions
-  command = command.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
-
   // Remove control characters and non-printable characters
   command = command.replace(/[\x00-\x1F\x7F-\x9F]/g, '');
 
-  // Prevent path escaping
+  // Remove special characters that could be used in exploits
+  command = command.replace(/[|`\\]/g, '');
+
+  // Prevent path traversal attacks by removing '..'
   command = command.replace(/\.\./g, '');
+
+  // Remove escaped character sequences like \xHH and \uHHHH
+  command = command.replace(/\\([0-9a-fA-F]{2})/g, '');
+
+  // Remove Unicode escape sequences like \uHHHH
+  command = command.replace(/\\u([0-9a-fA-F]{4})/g, '');
 
   return command;
 }
